bb01bee7059ed7d1fa24999b1b6be87d3e185a19
raflyakbarr <133252961+raflyakbarr@users.noreply.github.com>
2023-10-15 10:50:16 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { View, DrawerLayoutAndroid, StatusBar } from "react-native";
+import { View, DrawerLayoutAndroid, StatusBar, Modal, Text } from "react-native";
 import Header from "./components/header";
 import Button from "./components/button";
 import Separator from "./components/separator";
@@ -9,6 +9,7 @@ import Article from "./screens/article";
 // Functional Component
 const App = () => {
   // State Declaration
+  const [modalVisible, setModalVisible] = useState(false);
   const [page, setPage] = useState("list");
   // Ref Declaration
   const drawer = useRef(null);
@@ -30,6 +31,9 @@ const App = () => {
       <Button text="Article" onPress={() => changePage(drawer, "article")} />
       <Separator height={30} />
       <Button text="Close" onPress={() => drawer.current.closeDrawer()} />
+      <Separator height={30} />
+      <Button text="Buka Modal" onPress={() => setModalVisible(true)} />
+      <Separator height={30} />
     </View>
   );
 
@@ -45,6 +49,14 @@ const App = () => {
         <Header drawer={drawer} />
         {page == "list" ? <List /> : page == "article" ? <Article /> : null}
       </View>
+       <Modal visible={modalVisible} animationType="slide" transparent={false}>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <View style={{ backgroundColor: "white", padding: 20, borderRadius: 10 }}>
+            <Text>Konten Modalt</Text>
+            <Button text="Tutup Modal" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
     </DrawerLayoutAndroid>
   );
 };
